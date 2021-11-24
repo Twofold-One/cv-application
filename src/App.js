600626a8bdc5f28a1e8c2b7353f69ec097599e8b
Twofold-One <evan_geras@mail.ru>
2021-11-25 06:25:58 +0700

    all required styling is made

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './styles/App.css';
 import Header from './components/Header';
 import General from './components/General';
@@ -23,18 +24,55 @@ const theme = createTheme({
     },
 });
 
-function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <div className="App">
-                <Header />
-                <General />
-                <Education />
-                <Experience />
-                <OutputCV />
-            </div>
-        </ThemeProvider>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            personData: {
+                general: {
+                    photoURL: '',
+                    firstName: '',
+                    secondName: '',
+                    position: '',
+                    Phone: '',
+                    Address: '',
+                    Email: '',
+                },
+                education: [
+                    {
+                        institution: '',
+                        degree: '',
+                        subject: '',
+                        from: '',
+                        to: '',
+                    },
+                ],
+                experience: [
+                    {
+                        companyName: '',
+                        position: '',
+                        mainTasks: '',
+                        from: '',
+                        to: '',
+                    },
+                ],
+            },
+        };
+    }
+
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <Header />
+                    <General />
+                    <Education />
+                    <Experience />
+                    <OutputCV />
+                </div>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default App;
--- src/components/OutputCV.js
+++ src/components/OutputCV.js
@@ -8,7 +8,10 @@ class OutputCV extends React.Component {
                 <h1>Your CV</h1>
                 <div className="CV-header">
                     <div className="CV-header-photo">
-                        <img src="https://petsworlds.ru/images/sampledata/Profile_avatar_placeholder_large.png"></img>
+                        <img
+                            src="https://petsworlds.ru/images/sampledata/Profile_avatar_placeholder_large.png"
+                            alt="your face"
+                        ></img>
                     </div>
                     <div className="CV-header-info">
                         <h2>Evan Geras</h2>
@@ -31,25 +34,29 @@ class OutputCV extends React.Component {
                 </div>
                 <div className="CV-main">
                     <div className="CV-info">
-                        <h2>EDUCATION</h2>
-                        <div>
+                        <h2 className="CV-info-h2">EDUCATION</h2>
+                        <div className="CV-info-other">
                             <div>
                                 <h2>Institution name</h2>
                                 <p>Degree, Subject</p>
                             </div>
-                            <div>January 2020 to Present</div>
+                            <div>
+                                <p>January 2020 to Present</p>
+                            </div>
                         </div>
                     </div>
                     <div className="CV-info">
-                        <h2>EXPERIENCE</h2>
-                        <div>
+                        <h2 className="CV-info-h2">EXPERIENCE</h2>
+                        <div className="CV-info-other">
                             <div>
                                 <h2>Company name</h2>
                                 <p>Position</p>
                             </div>
-                            <div>January 2020 to Present</div>
+                            <div>
+                                <p>January 2020 to Present</p>
+                            </div>
                         </div>
-                        <div>
+                        <div className="CV-info-other-tasks">
                             <p>Main tasks</p>
                         </div>
                     </div>
